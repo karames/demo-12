c256bf7be33ceb7934a76cf62225d7aa05c0d719
Karames <karames@gmail.com>
2020-08-29 15:23:56 +0200

    prueba

---
+++ nono.js
@@ -0,0 +1,19 @@
+// INFO informacion
+// - informacion
+// HACER KKDKKDKKD
+// >  KKDKKDKKD
+// MEJORAR LDSKFJLKAJDF
+// & LDSKFJLKAJDF
+// DUDAR SALKJDFLKAJSFDLK
+// ? SALKJDFLKAJSFDLK
+// ERROR SDFLKSADJF
+// * SDFLKSADJF
+// REVISAR Estamos realizando una prueba no sabemos como funciona
+// ! SDLKJFLKSJDF
+// IGNORAR SDJFLKSJDF
+// // COMENTARIO NORMAL
+// COMENTARIO NORMAL Estamos realizanod una prueba
+console.log("hola");
+console.log("hola");
+console.log("hola");
+console.log("hola");
